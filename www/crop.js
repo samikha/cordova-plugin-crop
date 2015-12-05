aa414fc031a45796e3d317c0d12d514b8d9384b0
Sami Khawam <S.Khawam@gmail.com>
2015-12-05 12:25:43 -0800

    Added the missing fixed ration function

--- www/crop.js
+++ www/crop.js
@@ -1,19 +1,23 @@
 /* global cordova */
-var crop = module.exports = function cropImage (success, fail, image, options) {
+
+var crop_export = {}
+crop_export.crop = function cropImage (success, fail, image, options) {
   options = options || {}
   options.quality = options.quality || 100
   return cordova.exec(success, fail, 'CropPlugin', 'cropImage', [image, options])
 }
 
 
-var crop_fixed_ratio_800x600 = module.exports = function cropImageFixedRatio800x600 (success, fail, image, options) {
+crop_export.crop_fixed_ratio_800x600  = function cropImageFixedRatio800x600 (success, fail, image, options) {
   options = options || {}
   options.quality = options.quality || 100
   return cordova.exec(success, fail, 'CropPlugin', 'cropImageFixedRatio800x600', [image, options])
 }
 
-module.exports.promise = function cropAsync (image, options) {
+crop_export.promise = function cropAsync (image, options) {
   return new Promise(function (resolve, reject) {
     crop(resolve, reject, image, options)
   })
 }
+
+module.exports = crop_export;
